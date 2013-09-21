0443b86b1c2305624d1969059d69cbbb6a76452e
Joseph Kinsella <jkinsella@spiryx.net>
2013-09-21 01:31:25 -0400

    icons code to represent the new icons.

--- icons.h
+++ icons.h
@@ -12,6 +12,12 @@ extern const unsigned char edititem[];
 /* created by reswrap from file icons/incomplete.png */
 extern const unsigned char incomplete[];
 
+/* created by reswrap from file icons/mainicon.png */
+extern const unsigned char mainicon[];
+
+/* created by reswrap from file icons/mainiconsm.png */
+extern const unsigned char mainiconsm[];
+
 /* created by reswrap from file icons/markcomplete.png */
 extern const unsigned char markcomplete[];
 
@@ -21,11 +27,17 @@ extern const unsigned char markincomplete[];
 /* created by reswrap from file icons/newlist.png */
 extern const unsigned char newlist[];
 
+/* created by reswrap from file icons/newreminder.png */
+extern const unsigned char newreminder[];
+
 /* created by reswrap from file icons/openckl.png */
 extern const unsigned char openckl[];
 
 /* created by reswrap from file icons/quitchecklist.png */
 extern const unsigned char quitchecklist[];
+
+/* created by reswrap from file icons/remindericon.png */
+extern const unsigned char remindericon[];
 
 /* created by reswrap from file icons/saveckl.png */
 extern const unsigned char saveckl[];
